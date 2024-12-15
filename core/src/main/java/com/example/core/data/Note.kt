4f36befb2b1c806d2f7187b2cc3b86be9180f985
Joko Triyanto <jokotriyanto12345@gmail.com>
2024-12-15 09:58:11 +0700

    feat(core): add data

---
+++ core/src/main/java/com/example/core/data/Note.kt
@@ -0,0 +1,10 @@
+package com.example.core.data
+
+data class Note(
+    var title: String = "",
+    var content: String = "",
+    var creationTime: Long = 0L,
+    var updateTime: Long = 0L,
+    var id: Long = 0L,
+    var wordCount: Int = 0
+)
